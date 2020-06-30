266405d86a1da407afd0cf5a1bc51685744e08bd
sadgabriel <66413143+sadgabriel@users.noreply.github.com>
2020-06-30 15:18:07 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,17 +5,11 @@ import copy
 
 os.environ['SDL_VIDEO_CENTERED'] = '1'
 
-
 LINECOLOR = (15, 30, 170)
 colormap = {2: (234, 242, 255), 4: (213, 228, 255), 8: (191, 216, 255), 16: (170, 202, 255),
             32: (149, 189, 255), 64: (128, 175, 255), 128: (106, 163, 255), 256: (85, 149, 255), 512: (64, 136, 225),
             1024: (43, 122, 255), 2048: (21, 109, 255), 4096: (0, 96, 255), 8192: (0, 88, 234), 16384: (0, 80, 213),
             32768: (0, 72, 191), 65536: (0, 64, 170), 131072: (0, 56, 149)}
-
-
-class GameOverSignal(Exception):
-    def __init__(self, string):
-        super().__init__(string)
 
 
 class Board:
@@ -28,36 +22,23 @@ class Board:
         self.spawn()
         self.render()
 
-    def is_empty(self, x, y):
-        if self.coordinate[x][y] is None:
-            return True
-        return False
-
     def spawn(self):
         while True:
             x = random.randrange(4)
             y = random.randrange(4)
 
-            if self.is_empty(x, y):
+            if self.coordinate[x][y] is None:
                 if random.randrange(2) == 0:
                     self.coordinate[x][y] = 2
                 else:
                     self.coordinate[x][y] = 4
                 break
-            else:
-                game_over = True
-                for x in range(4):
-                    for y in range(4):
-                        if self.is_empty(x, y):
-                            game_over = False
-                if game_over:
-                    raise GameOverSignal('Game Over')
 
     def render(self):
         self.screen.fill(LINECOLOR)
         for x in range(4):
             for y in range(4):
-                if self.is_empty(x, y):
+                if self.coordinate[x][y] is None:
                     color = (255, 255, 255)
                 else:
                     color = colormap[self.coordinate[x][y]]
@@ -68,12 +49,30 @@ class Board:
                     self.screen.blit(surface, rect)
         pygame.display.flip()
 
+    def check(self):
+        count = 0
+        test_board = Board(self.screen.copy())
+        test_board.coordinate = copy.deepcopy(self.coordinate)
+        if not test_board.up():
+            count += 1
+        test_board.coordinate = copy.deepcopy(self.coordinate)
+        if not test_board.down():
+            count += 1
+        test_board.coordinate = copy.deepcopy(self.coordinate)
+        if not test_board.left():
+            count += 1
+        test_board.coordinate = copy.deepcopy(self.coordinate)
+        if not test_board.right():
+            count += 1
+        if count == 4:
+            return True
+
     def up(self):
         stable = True
         for x in range(4):
             empty_phase = False
             for y in range(4):
-                if self.is_empty(x, y):
+                if self.coordinate[x][y] is None:
                     empty_phase = True
                 elif empty_phase:
                     stable = False
@@ -83,7 +82,7 @@ class Board:
             new_list = list()
 
             for y in range(4):
-                if not self.is_empty(x, y):
+                if self.coordinate[x][y] is not None:
                     old_list.append(self.coordinate[x][y])
 
             while old_list:
@@ -104,17 +103,19 @@ class Board:
                 self.coordinate[x][y] = new_list[y]
 
         if stable:
-            raise GameOverSignal('Game Over')
-        else:
-            self.spawn()
-            self.render()
+            return False
+
+        self.spawn()
+        self.render()
+
+        return True
 
     def down(self):
         stable = True
-        for x in range(3, -1, -1):
+        for x in range(4):
             empty_phase = False
             for y in range(3, -1, -1):
-                if self.is_empty(x, y):
+                if self.coordinate[x][y] is None:
                     empty_phase = True
                 elif empty_phase:
                     stable = False
@@ -124,7 +125,7 @@ class Board:
             new_list = list()
 
             for y in range(3, -1, -1):
-                if not self.is_empty(x, y):
+                if self.coordinate[x][y] is not None:
                     old_list.append(self.coordinate[x][y])
 
             while old_list:
@@ -145,17 +146,19 @@ class Board:
                 self.coordinate[x][3-y] = new_list[y]
 
         if stable:
-            raise GameOverSignal('Game Over')
-        else:
-            self.spawn()
-            self.render()
+            return False
+
+        self.spawn()
+        self.render()
+
+        return True
 
     def left(self):
         stable = True
         for y in range(4):
             empty_phase = False
             for x in range(4):
-                if self.is_empty(x, y):
+                if self.coordinate[x][y] is None:
                     empty_phase = True
                 elif empty_phase:
                     stable = False
@@ -165,7 +168,7 @@ class Board:
             new_list = list()
 
             for x in range(4):
-                if not self.is_empty(x, y):
+                if self.coordinate[x][y] is not None:
                     old_list.append(self.coordinate[x][y])
 
             while old_list:
@@ -186,17 +189,19 @@ class Board:
                 self.coordinate[x][y] = new_list[x]
 
         if stable:
-            raise GameOverSignal('Game Over')
-        else:
-            self.spawn()
-            self.render()
+            return False
+
+        self.spawn()
+        self.render()
+
+        return True
 
     def right(self):
         stable = True
-        for y in range(3, -1, -1):
+        for y in range(4):
             empty_phase = False
             for x in range(3, -1, -1):
-                if self.is_empty(x, y):
+                if self.coordinate[x][y] is None:
                     empty_phase = True
                 elif empty_phase:
                     stable = False
@@ -206,7 +211,7 @@ class Board:
             new_list = list()
 
             for x in range(3, -1, -1):
-                if not self.is_empty(x, y):
+                if self.coordinate[x][y] is not None:
                     old_list.append(self.coordinate[x][y])
 
             while old_list:
@@ -227,10 +232,12 @@ class Board:
                 self.coordinate[3-x][y] = new_list[x]
 
         if stable:
-            raise GameOverSignal('Game Over')
-        else:
-            self.spawn()
-            self.render()
+            return False
+
+        self.spawn()
+        self.render()
+
+        return True
 
 
 def main():
@@ -241,65 +248,40 @@ def main():
     board = Board(screen)
 
     game_over = False
-
+    signal = False
     running = True
     while running:
-        try:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    running = False
-                elif event.type == pygame.KEYDOWN:
-                    if game_over and event.key == pygame.K_SPACE:
-                        board = Board(screen)
-                        game_over = False
-                    elif event.key == pygame.K_UP:
-                        board.up()
-                    elif event.key == pygame.K_DOWN:
-                        board.down()
-                    elif event.key == pygame.K_LEFT:
-                        board.left()
-                    elif event.key == pygame.K_RIGHT:
-                        board.right()
-        except GameOverSignal:
-            raised_error = 0
-            try:
-                test_board = Board(pygame.Surface((425, 425)))
-                test_board.coordinate = board.coordinate.copy()
-                test_board.up()
-            except GameOverSignal:
-                raised_error += 1
-            try:
-                test_board = Board(pygame.Surface((425, 425)))
-                test_board.coordinate = board.coordinate.copy()
-                test_board.down()
-            except GameOverSignal:
-                raised_error += 1
-            try:
-                test_board = Board(pygame.Surface((425, 425)))
-                test_board.coordinate = board.coordinate.copy()
-                test_board.left()
-            except GameOverSignal:
-                raised_error += 1
-            try:
-                test_board = Board(pygame.Surface((425, 425)))
-                test_board.coordinate = board.coordinate.copy()
-                test_board.right()
-            except GameOverSignal:
-                raised_error += 1
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.KEYDOWN:
+                if game_over and event.key == pygame.K_SPACE:
+                    board = Board(screen)
+                    game_over = False
+                elif event.key == pygame.K_UP:
+                    if board.up() and board.check():
+                        signal = True
+                elif event.key == pygame.K_DOWN:
+                    if board.down() and board.check():
+                        signal = True
+                elif event.key == pygame.K_LEFT:
+                    if board.left() and board.check():
+                        signal = True
+                elif event.key == pygame.K_RIGHT:
+                    if board.right() and board.check():
+                        signal = True
 
-            if raised_error == 4:
-                game_over = True
-                surface = pygame.font.Font('arial.ttf', 65).render('Game Over', True, (0, 0, 0))
-                rect = surface.get_rect(center=(212, 180))
-                screen.blit(surface, rect)
+        if signal:
+            signal = False
+            game_over = True
+            surface = pygame.font.Font('arial.ttf', 65).render('Game Over', True, (0, 0, 0))
+            rect = surface.get_rect(center=(212, 180))
+            screen.blit(surface, rect)
 
-                surface = pygame.font.Font('arial.ttf', 25).render('Press space', True, (0, 0, 0))
-                rect = surface.get_rect(center=(212, 240))
-                screen.blit(surface, rect)
-                pygame.display.flip()
-
-
-
+            surface = pygame.font.Font('arial.ttf', 25).render('Press space', True, (0, 0, 0))
+            rect = surface.get_rect(center=(212, 240))
+            screen.blit(surface, rect)
+            pygame.display.flip()
 
 
 if __name__ == '__main__':
